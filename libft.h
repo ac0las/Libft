37aa410c615e72ac35fe9bfc71a40416c335c17f
Antonio Colas Lafuente <acolas-l@c3r7s5.42madrid.com>
2023-09-27 12:10:36 +0200

    subido

---
+++ libft.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acolas-l <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/22 17:38:55 by acolas-l          #+#    #+#             */
+/*   Updated: 2023/09/22 18:11:11 by acolas-l         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <unistd.h>
+# include <stlib.h>
+# include <string.h>
+
+int ft_isalpha (int c);
+int ft_isdigit (int c);
+int ft_alnum (int c);
+int ft_isascii (int c);
+
+#endif
