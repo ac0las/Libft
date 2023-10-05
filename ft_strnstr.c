f896298f493adf69e0cec31041f61a40702e3a8f
Antonio Colas Lafuente <acolas-l@c2r6s5.42madrid.com>
2023-10-05 19:57:33 +0200

    subido

---
+++ ft_strnstr.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   strnstr.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acolas-l <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/05 19:06:29 by acolas-l          #+#    #+#             */
+/*   Updated: 2023/10/05 19:56:20 by acolas-l         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <libft.h>
+
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	size_t	needle_len;
+	size_t	i;
+
+	needle_len = ft_strlen(needle);
+	if (needle_len == 0)
+		return ((char *)haystack);
+	i = 0;
+	while (i <= len - needle_len)
+	{
+		if (haystack[i] == needle [0]
+			&& ft_strncmp(&haystack[i], needle, needle_len) == 0)
+			return ((char *)&haystack[i]);
+		i++;
+	}
+	return (NULL);
+}
