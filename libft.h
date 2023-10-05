d3d0b0bc512d07f2ac2de6b4be8cd1016ac4cd1b
Antonio Colas Lafuente <acolas-l@c2r6s5.42madrid.com>
2023-10-05 17:02:41 +0200

    subido

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: acolas-l <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/22 17:38:55 by acolas-l          #+#    #+#             */
-/*   Updated: 2023/09/22 18:11:11 by acolas-l         ###   ########.fr       */
+/*   Updated: 2023/10/02 19:37:12 by acolas-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,16 @@
 # include <stlib.h>
 # include <string.h>
 
-int ft_isalpha (int c);
-int ft_isdigit (int c);
-int ft_alnum (int c);
-int ft_isascii (int c);
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_alnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strrchr(const char *s, int c);
+size_t	ft_strlen(const char *s);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
 
 #endif
